19d240c9fdfb3539e52f912031b3c142c7f5d221
em1ttal <eshaan.mittal1@gmail.com>
2024-11-11 15:52:56 +0100

    añadir metodo para recuperar los datos de una pelicula

---
+++ backend/supabase/tests/movie-info_tests.py
@@ -0,0 +1,31 @@
+import os
+from uuid import UUID
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+# Load .env file from the parent directory
+load_dotenv(os.path.join(os.path.dirname(__file__), '../../../.env'))
+
+# Now you can access your environment variables
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Contador de aserciones pasadas y fallidas
+asserts_passed = 0
+asserts_failed = 0
+total_asserts = 5  # Cambia este número si agregas más pruebas
+
+# Pruebas
+
+# - Movie Details
+
+try:
+    movie_id = 12
+    response = supabase.rpc("get_movie_details", {'movie_id': movie_id}).execute()
+    assert response.data[0] == {'id': 12, 'title': 'Buscando a Nemo', 'release_date': '2003-05-30', 'overview': 'Nemo, un pececillo, hijo único muy querido y protegido por su padre, ha sido capturado en un arrecife australiano y ahora vive en una pecera en la oficina de un dentista de Sidney. El tímido padre de Nemo se embarcará en una peligrosa aventura para rescatar a su hijo. Pero Nemo y sus nuevos amigos tienen también un astuto plan para escapar de la pecera y volver al mar.', 'vote_average': 7.819, 'poster_url': 'https://image.tmdb.org/t/p/original/jPhak722pNGxQIXSEfeWIUqBrO5.jpg'}
+    asserts_passed +=1
+except AssertionError:
+    asserts_failed +=1
+
