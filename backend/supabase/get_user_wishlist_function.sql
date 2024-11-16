635f318f856b10c8ea28361e0195484030d4a237
em1ttal <eshaan.mittal1@gmail.com>
2024-11-16 18:37:03 +0100

    tuve que actualizar la funcionalidad para los tests

--- backend/supabase/get_user_wishlist_function.sql
+++ backend/supabase/get_user_wishlist_function.sql
@@ -1,15 +1,15 @@
-create or replace function get_wishlist(user_id uuid)
-returns setof "Peliculas" as $$
-begin
-  if not exists ( select 1 from auth.users where auth.users.id = user_id) THEN 
-    return;
-  else
-    return query ( 
-      select id, title, genre, release_date, year, poster_url, overview, "cast", vote_average, vote_count, director_fk, language_fk
-      from "Peliculas"
-      inner join "Wishlist" on "Wishlist".movie_fk = "Peliculas".id
-      where "Wishlist".user_fk = user_id
-    );
-  end if;
-end
-$$ language plpgsql;
+CREATE OR REPLACE FUNCTION get_wishlist(user_id uuid)
+RETURNS TABLE (
+  id bigint,
+  title text,
+  poster_url text,
+  vote_average float,
+  vote_count int
+)
+LANGUAGE sql
+AS $$
+  SELECT p.id, p.title, p.poster_url, p.vote_average, p.vote_count
+  FROM public."Peliculas" p
+  INNER JOIN public."Wishlist" w ON w.movie_fk = p.id
+  WHERE w.user_fk = user_id;
+$$;
