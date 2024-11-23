a60b3a1067c9263a9104c590f9749208a14380c6
Jose Javier Iglesias <josejavieriglesiasm@gmail.com>
2024-11-23 16:12:38 +0100

    New navegation to forum

---
+++ frontend/pages/movies/[id]/forum/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Forum for Movie ID: {{ movieId }}</h1>
+      <p>Welcome to the forum for {{ movieTitle }}.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+  const movieId = route.query.id; // Access movie ID from query params
+  const movieTitle = route.params.id; // Access movie title from route params
+  </script>
+  
---
+++ frontend/pages/movies/[id]/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <MovieDetails></MovieDetails>
+    <NuxtLink :to="forumLink">Go to Forum</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Movie',
+  meta: [{ name: 'description', content: 'Movie' }],
+});
+
+definePageMeta({
+  layout: 'home',
+});
+
+const route = useRoute();
+const movieId = route.query.id; // Movie ID from query
+const movieTitle = route.params.id; // Movie title from params
+const forumLink = `/movies/${movieTitle}/forum?id=${movieId}`; // Build forum link
+</script>
