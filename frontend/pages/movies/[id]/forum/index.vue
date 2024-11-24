377e8d886a6269dd372ef0db8b476670f5f66495
ronaldortiz99 <ronald9qva@gmail.com>
2024-11-24 15:22:49 +0100

    Conexión frontend y backend

--- frontend/pages/movies/[id]/forum/index.vue
+++ frontend/pages/movies/[id]/forum/index.vue
@@ -1,16 +1,17 @@
 <template>
     <main>
       <section>
-        <div v-if="!movieTitle" class="text-center">
+        <div v-if="!movieTitle || forum_exist === null" class="text-center">
           <p class="text-gray-500 text-lg">Loading forum...</p>
         </div>
   
         <div v-else>
-
-          <!-- Forum Header -->
+          <div v-if="forum_exist">
+            <!-- Forum Header -->
             <div class="flex justify-between items-center mb-6">
                 <div>
-                    <h1 class="text-4xl font-bold">{{ movieTitle }}</h1>
+                    <h1 class="text-4xl font-bold">{{ forum[0]?.name }}</h1>
+                    <h3 class="">{{ forum[0]?.description }}</h3>
                 </div>
                 <div>
                     <UButton 
@@ -22,10 +23,14 @@
                     </UButton>
                 </div>
             </div>
-          <!-- Posts Container -->
-          <div class="container mx-auto mt-6">
-            <div class="space-y-6"> 
+            <!-- Posts Container -->
+            <div class="container mx-auto mt-6">
+              <div class="space-y-6"> 
+              </div>
             </div>
+          </div>
+          <div v-else>
+              <h1 class="text-4xl font-bold"> El Foro de la película {{ movieTitle }} aún no está disponible.</h1>
           </div>
         </div>
       </section>
@@ -35,6 +40,7 @@
   <script setup>
   import { useRoute } from 'vue-router';
   import { useSupabaseUser } from '#imports';
+  import { ref, onMounted } from 'vue';
   
   useHead({
     title: 'Forum',
@@ -46,9 +52,41 @@
   });
   
   const route = useRoute();
+  const client = useSupabaseAuthClient()
   const user = useSupabaseUser();
   const movieId = route.query.id; // Access movie ID from query params
   const movieTitle = route.params.id; // Access movie title from route params
+
+
+  const forum = ref([]);
+  const forum_exist = ref(null)
+  
+
+  const fetchExistsForum = async () => {
+    const { data, error } = await client.rpc('exists_forum',{input_movie_id: parseInt(movieId, 10)});
+
+    if (error) {
+      console.error('Error al comprobar si el foro existe:', error);
+    } else {
+      forum_exist.value = data;
+      if (data){
+        fetchForum();
+      }
+    }
+  };
+
+
+  const fetchForum = async () => {
+    const { data, error } = await client.rpc('get_forum_info',{input_movie_id: parseInt(movieId, 10)});
+
+    if (error) {
+      console.error('Error al obtener la info de forum:', error);
+    } else {
+      forum.value = data;
+    }
+  };
+
+  onMounted(fetchExistsForum);
   </script>
   
   
