847b5c7a4c871738fea59be3e48cea55405111de
em1ttal <eshaan.mittal1@gmail.com>
2024-12-01 11:39:30 +0100

    actualizar sql

--- backend/supabase/add_movie_rating.sql
+++ backend/supabase/add_movie_rating.sql
@@ -1,22 +1,21 @@
-create or replace function ratemovie(user_id uuid, movie_id bigint, rating int)
+create or replace function ratemovie(user_id uuid, movie_id bigint, new_rating int)
 returns table(success boolean, message text) as $$
 begin
-    IF rating < 1 OR rating > 5 THEN
-        RETURN query value (false, 'Rating value is incorrect';)
-    END IF;
-  if not  exists (select 1 from "Peliculas" where "Peliculas".id = movie_id) THEN 
-    return query values (false, 'Movie does not exist');
-  elsif not exists ( select 1 from auth.users where auth.users.id = user_id) THEN 
-    return query values (false, 'User does not exist');
+  if new_rating < 1 OR new_rating > 5 THEN
+      return query values (false, 'Rating must be between 1 and 5.');
+  elsif not  exists (select 1 from "Peliculas" where "Peliculas".id = movie_id) THEN 
+    return query values (false, 'Movie does not exist.');
+  elsif not exists ( select 1 from public."Usuarios Auth" where public."Usuarios Auth".id = user_id) THEN 
+    return query values (false, 'User does not exist.');
   elsif exists ( select 1 from "Valoraciones" where user_fk = user_id and movie_fk = movie_id) THEN 
-    UPDATE "Valoraciones" v
-    SET v.rating = rating
-    WHERE v.user_fk = user_id AND v.movie_fk = movie_id;
-    return query values (true, 'Rating updated successfully');
+    UPDATE "Valoraciones" 
+    SET rating = new_rating
+    WHERE user_fk = user_id AND movie_fk = movie_id;
+    return query values (true, 'Rating added successfully.');
   else
     INSERT INTO "Valoraciones" (user_fk, movie_fk, rating)
-    values (user_id, movie_id, rating);
-    return query values (true, 'Rating addedd successfully');
+    values (user_id, movie_id, new_rating);
+    return query values (true, 'Rating updated successfully.');
   end if;
 end
 $$ language plpgsql;
