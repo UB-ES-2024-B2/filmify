0ab31060b3da5ef5c976d8351e687f1536278e65
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2024-12-02 21:41:16 +0100

    Update posts_tests.py

--- backend/supabase/tests/posts_tests.py
+++ backend/supabase/tests/posts_tests.py
@@ -1,88 +1,59 @@
 import os
+import pytest
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
-# Load .env file from the parent directory
-load_dotenv(os.path.join(os.path.dirname(__file__), '../../../.env'))
+@pytest.fixture(scope='module')
+def db_connection():
+    # Load .env file from the parent directory
+    load_dotenv(os.path.join(os.path.dirname(__file__), '../../../.env'))
 
-# Now you can access your environment variables
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    # Now you can access your environment variables
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# Contador de aserciones pasadas y fallidas
-asserts_passed = 0
-asserts_failed = 0
-total_asserts = 7  # Cambia este número si agregas más pruebas
+    return supabase 
 
-# Pruebas
+# Delete posts de Usuario Test en un Foro
+def test_posts_delete(db_connection):
+    response = db_connection.rpc("delete_posts_from_user", {'input_movie_id': 77, 'input_user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
+    assert response.data
 
-# - Delete posts de Usuario Test en un Foro
-try:
-    responseDelete = supabase.rpc("delete_posts_from_user", {'input_movie_id': 77, 'input_user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
-    assert responseDelete.data
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en eliminación de posts")
+# Posts de Foro Vacío
+def test_posts_get1(db_connection):
+    response = db_connection.rpc("get_posts", {'input_movie_id': 77}).execute()
+    assert len(response.data) == 0
 
-# - Posts de Foro Vacío
-try:
-    responseGet1 = supabase.rpc("get_posts", {'input_movie_id': 77}).execute()
-    assert len(responseGet1.data) == 0
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en obtener posts (vacío)")
+# Create Post
+def test_post_create1(db_connection):
+    response = db_connection.rpc("create_post", {'title': 'Título de Pueba', 'content': 'Contenido de prueba', 'input_movie_id': 77, 'user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
+    assert response.data
 
-# - Create Post
-try:
-    responseCreate1 = supabase.rpc("create_post", {'title': 'Título de Pueba', 'content': 'Contenido de prueba', 'input_movie_id': 77, 'user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
-    assert responseCreate1.data
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en crear post 1")
+# Create Post de Película que no exite
+def test_post_create2(db_connection):
+    response = db_connection.rpc("create_post", {'title': 'Título de Pueba', 'content': 'Contenido de prueba', 'input_movie_id': 1, 'user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
+    assert response.data == False
 
-# - Create Post de Película que no existe
-try:
-    responseCreate2 = supabase.rpc("create_post", {'title': 'Título de Pueba', 'content': 'Contenido de prueba', 'input_movie_id': 1, 'user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
-    assert responseCreate2.data == False
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en crear post con película inexistente")
+# Create Post con Usuario equivocado
+def test_post_create3(db_connection):
+    response = db_connection.rpc("create_post", {'title': 'Título de Pueba', 'content': 'Contenido de prueba', 'input_movie_id': 1, 'user_id': '8a30b414-3548-0000-98de-1f849a77ef81'}).execute()
+    assert response.data == False
 
-# - Create Post con Usuario equivocado
-try:
-    responseCreate3 = supabase.rpc("create_post", {'title': 'Título de Pueba', 'content': 'Contenido de prueba', 'input_movie_id': 1, 'user_id': '8a30b414-3548-0000-98de-1f849a77ef81'}).execute()
-    assert responseCreate3.data == False
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en crear post con usuario incorrecto")
+# Create Post 2
+def test_post_create4(db_connection):
+    response = db_connection.rpc("create_post", {'title': 'Título de Pueba 2', 'content': 'Contenido de prueba 2', 'input_movie_id': 77, 'user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
+    assert response.data
 
-# - Create Post 2
-try:
-    responseCreate4 = supabase.rpc("create_post", {'title': 'Título de Pueba 2', 'content': 'Contenido de prueba 2', 'input_movie_id': 77, 'user_id': '8a30b414-3548-434a-98de-1f849a77ef81'}).execute()
-    assert responseCreate4.data
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en crear post 2")
 
-# - Posts de Foro
-try:
-    responseGet2 = supabase.rpc("get_posts", {'input_movie_id': 77}).execute()
-    assert len(responseGet2.data) == 2
-    assert responseGet2.data[0]['title'] == 'Título de Pueba'
-    assert responseGet2.data[0]['content'] == 'Contenido de prueba'
-    assert responseGet2.data[0]['username'] == 'testuser'
-    assert responseGet2.data[1]['title'] == 'Título de Pueba 2'
-    assert responseGet2.data[1]['content'] == 'Contenido de prueba 2'
-    assert responseGet2.data[1]['username'] == 'testuser'
-    asserts_passed += 1
-except AssertionError:
-    asserts_failed += 1
-    print("Fallo en obtener posts (con datos)")
+# Posts de Foro 
+def test_posts_get2(db_connection):
+    response = db_connection.rpc("get_posts", {'input_movie_id': 77}).execute()
+    assert len(response.data) == 2
+    assert response.data[0]['title'] == 'Título de Pueba'
+    assert response.data[0]['content'] == 'Contenido de prueba'
+    assert response.data[0]['username'] == 'testuser'
+    assert response.data[1]['title'] == 'Título de Pueba 2'
+    assert response.data[1]['content'] == 'Contenido de prueba 2'
+    assert response.data[1]['username'] == 'testuser'
